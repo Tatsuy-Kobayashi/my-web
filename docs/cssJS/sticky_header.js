49268070fc62728dd5206e50ca2892dba4f587c8
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-10-16 20:27:15 +0900

    website sticky header

---
+++ docs/cssJS/sticky_header.js
@@ -0,0 +1,8 @@
+var checkBox = document.getElementsById("stickyToggle")[0];
+if(checkBox.checked){
+    // ここにチェックボックスにチェックがあるときの処理を記述
+    alert("チェックボックスにチェックが入っています");
+}else{
+    // ここにチェックボックスにチェックがないときの処理を記述
+    alert("チェックボックスにチェックが入っていません");
+}

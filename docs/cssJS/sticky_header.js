7d91208694bd87ff46189c8eddb604a98c5f6538
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-10-17 20:37:44 +0900

    website sticky header

--- docs/cssJS/sticky_header.js
+++ docs/cssJS/sticky_header.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const stickyToggle = document.getElementById("stickyToggle");
     const header = document.querySelector("header");
 
-    stickyToggle.addEventListener('change', function () {
+    stickyToggle.addEventListener('click', function () {
         if (this.checked) {
             header.classList.add('sticky');
         } else {

d92bace82b627123e5fedf6b4955530ea9a2a3c5
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-10-18 00:16:28 +0900

    website sticky header

--- docs/cssJS/sticky_header.js
+++ docs/cssJS/sticky_header.js
@@ -1,17 +1,10 @@
 $(document).ready(function() {
     // Ajaxでヘッダーをロードしてからイベントリスナーを設定
     $("header").load("header.html", function() {
-        const $stickyToggle = $("#stickyToggle");
         const $header = $("header");
 
-        if ($stickyToggle.length === 0) {
-            console.error("stickyToggle is not found in the DOM");
-        } else {
-            console.log("stickyToggle element found:", $stickyToggle);
-        }
-
-        // チェックボックスの状態に応じてクラスを追加・削除
-        $stickyToggle.on('click', function() {
+        // 動的に生成された#stickyToggleにもイベントをバインド
+        $(document).on('click', '#stickyToggle', function() {
             console.log("Checkbox clicked: ", $(this).prop('checked'));  // $(this).prop('checked') を使用
 
             if ($(this).prop('checked')) {

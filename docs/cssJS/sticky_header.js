912d2211d34cfb5cb67b66be4cde3bf5d9228765
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-10-18 00:49:48 +0900

    website sticky header

--- docs/cssJS/sticky_header.js
+++ docs/cssJS/sticky_header.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
+    /*
+    const $stickyToggle = $("#stickyToggle");
+    const $header = $("header");
+
+    $stickyToggle.on('change', function() {
+    */
+
     // Ajaxでヘッダーをロードしてからイベントリスナーを設定
     $("header").load("header.html", function() {
+        // const $stickyToggle = $("#stickyToggle");
         const $header = $("header");
 
+        // しかし、load()メソッドは非同期にheader.htmlをロードするため、要素のクリックイベントのバインディングがうまくいかない場合がある。
+        // なので、on()メソッドを使って動的に追加された要素にも対応できるようにする。
         // 動的に生成された#stickyToggleにもイベントをバインド
         $(document).on('click', '#stickyToggle', function() {
+            /*
             console.log("Checkbox clicked: ", $(this).prop('checked'));  // $(this).prop('checked') を使用
+            */
 
             if ($(this).prop('checked')) {
                 $header.addClass('sticky_header');

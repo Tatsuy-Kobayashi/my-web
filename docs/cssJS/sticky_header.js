5d947c5434c711fb2755f57affa7cf2360538769
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-10-17 23:08:54 +0900

    website sticky header

--- docs/cssJS/sticky_header.js
+++ docs/cssJS/sticky_header.js
@@ -1,30 +1,24 @@
 $(document).ready(function() {
-    const $stickyToggle = $("#stickyToggle");
-    const $header = $("header");
+    // Ajaxでヘッダーをロードしてからイベントリスナーを設定
+    $("header").load("header.html", function() {
+        const $stickyToggle = $("#stickyToggle");
+        const $header = $("header");
 
-    if ($stickyToggle.length === 0) {
-        console.error("stickyToggle is not found in the DOM");
-    } else {
-        console.log("stickyToggle element found:", $stickyToggle);
-    }
-
-    if ($header.length === 0) {
-        console.error("header is not found in the DOM");
-    } else {
-        console.log("header element found:", $header);
-    }
-
-    if (!$stickyToggle) {
-        return false;
-    }
-
-    $stickyToggle.on('change', function() {
-        console.log("Checkbox clicked: ", this.checked);
-
-        if (this.checked) {
-            $header.addClass('sticky');
+        if ($stickyToggle.length === 0) {
+            console.error("stickyToggle is not found in the DOM");
         } else {
-            $header.removeClass('sticky');
+            console.log("stickyToggle element found:", $stickyToggle);
         }
+
+        // チェックボックスの状態に応じてクラスを追加・削除
+        $stickyToggle.on('change', function() {
+            console.log("Checkbox clicked: ", this.checked);
+
+            if (this.checked) {
+                $header.addClass('sticky');
+            } else {
+                $header.removeClass('sticky');
+            }
+        });
     });
 });

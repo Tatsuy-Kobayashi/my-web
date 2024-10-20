0cf28b285810163485e1a769134e8fcfbf8e90d6
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-10-20 15:23:17 +0900

    website sticky haeder window size

--- docs/cssJS/sticky_header.js
+++ docs/cssJS/sticky_header.js
@@ -7,23 +7,36 @@ $(document).ready(function() {
     */
 
     // Ajaxでヘッダーをロードしてからイベントリスナーを設定
-    $("header").load("header.html", function() {
-        // const $stickyToggle = $("#stickyToggle");
-        const $header = $("header");
+    function handleStickyHeader() {
+        // 画面の横幅を取得
+        var windowWidth = $(window).width();
+        if (windowWidth > 768) {
+            $("header").load("header.html", function() {
+                // const $stickyToggle = $("#stickyToggle");
+                const $header = $("header");
 
-        // しかし、load()メソッドは非同期にheader.htmlをロードするため、要素のクリックイベントのバインディングがうまくいかない場合がある。
-        // なので、on()メソッドを使って動的に追加された要素にも対応できるようにする。
-        // 動的に生成された#stickyToggleにもイベントをバインド
-        $(document).on('click', '#stickyToggle', function() {
-            /*
-            console.log("Checkbox clicked: ", $(this).prop('checked'));  // $(this).prop('checked') を使用
-            */
+                // しかし、load()メソッドは非同期にheader.htmlをロードするため、要素のクリックイベントのバインディングがうまくいかない場合がある。
+                // なので、on()メソッドを使って動的に追加された要素にも対応できるようにする。
+                // 動的に生成された#stickyToggleにもイベントをバインド
+                $(document).on('click', '#stickyToggle', function() {
+                    /*
+                    console.log("Checkbox clicked: ", $(this).prop('checked'));  // $(this).prop('checked') を使用
+                    */
+                    if ($(this).prop('checked')) {
+                        $header.addClass('sticky_header');
+                    } else {
+                        $header.removeClass('sticky_header');
+                    }
+                });
+            });
+        }
+    }
 
-            if ($(this).prop('checked')) {
-                $header.addClass('sticky_header');
-            } else {
-                $header.removeClass('sticky_header');
-            }
-        });
+    // 初回実行
+    handleStickyHeader();
+
+    // 画面サイズ変更時にもチェック
+    $(window).resize(function() {
+        handleStickyHeader();
     });
 });

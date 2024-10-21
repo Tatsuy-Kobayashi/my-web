17779b5456e0d455d6ba57d24dc7956de10345cb
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-10-21 21:38:13 +0900

    website hamburger

--- docs/cssJS/drawer.js
+++ docs/cssJS/drawer.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
-    $('#open_nav').on('click', function() {
-        $('#wrapper, #nav').toggleClass('show');
-    });
+document.getElementById('menu-btn').addEventListener('click', function() {
+    // ハンバーガーメニューの表示・非表示を制御
+    const drawerNav = document.getElementById('drawerNav');
+    if (this.checked) {
+        drawerNav.style.left = '0'; // 表示
+    } else {
+        drawerNav.style.left = '-250px'; // 隠す
+    }
 });

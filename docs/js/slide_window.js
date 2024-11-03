a0133a017ec9d01bcf770f08cbdfa25c4d8d2d66
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-11-03 23:07:20 +0900

    website

--- docs/js/slide_window.js
+++ docs/js/slide_window.js
@@ -1,36 +1,38 @@
-const $next = document.querySelector('.next');
-const $prev = document.querySelector('.prev');
-let autoSlideInterval;
+$(document).ready(function () {
+    const $next = document.querySelector('.next');
+    const $prev = document.querySelector('.prev');
+    let autoSlideInterval;
 
-// Function to show the next slide
-function showNextSlide() {
-    const items = document.querySelectorAll('.item');
-    document.querySelector('.slide').appendChild(items[0]);
-}
+    // Function to show the next slide
+    function showNextSlide() {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(items[0]);
+    }
 
-// Function to show the previous slide
-function showPrevSlide() {
-    const items = document.querySelectorAll('.item');
-    document.querySelector('.slide').prepend(items[items.length - 1]);
-}
+    // Function to show the previous slide
+    function showPrevSlide() {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(items[items.length - 1]);
+    }
 
-// Button click event listeners
-$next.addEventListener('click', showNextSlide);
-$prev.addEventListener('click', showPrevSlide);
+    // Button click event listeners
+    $next.addEventListener('click', showNextSlide);
+    $prev.addEventListener('click', showPrevSlide);
 
-// Start the auto-slide with a 1-second interval
-function startAutoSlide() {
-    autoSlideInterval = setInterval(showNextSlide, 9000);
-}
+    // Start the auto-slide with a 1-second interval
+    function startAutoSlide() {
+        autoSlideInterval = setInterval(showNextSlide, 9000);
+    }
 
-// Stop the auto-slide
-function stopAutoSlide() {
-    clearInterval(autoSlideInterval);
-}
+    // Stop the auto-slide
+    function stopAutoSlide() {
+        clearInterval(autoSlideInterval);
+    }
 
-// Start auto-slide on page load
-startAutoSlide();
+    // Start auto-slide on page load
+    startAutoSlide();
 
-// Pause auto-slide on button hover
-document.querySelector('.button_slide').addEventListener('mouseover', stopAutoSlide);
-document.querySelector('.button_slide').addEventListener('mouseout', startAutoSlide);
+    // Pause auto-slide on button hover
+    document.querySelector('.button_slide').addEventListener('mouseover', stopAutoSlide);
+    document.querySelector('.button_slide').addEventListener('mouseout', startAutoSlide);
+});

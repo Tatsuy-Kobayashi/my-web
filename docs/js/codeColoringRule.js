4880c0eedbba286153ecb1fbce080b6830814bd3
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-11-05 01:37:50 +0900

    website codeColoringRule

--- docs/js/codeColoringRule.js
+++ docs/js/codeColoringRule.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     const patterns = {
         python: [
+            { type: 'number', regex: /(?<=\s|[(),+\-*/])(\d+(\.\d+)?|\.\d+)(?=\s|[<(),+\-*/])/g },
             { type: 'string', regex: /(["'`].*?["'`])/g },
             { type: 'structure', regex: /\b(import|from|return|for|as|if|else|elif|match|continue|while)\b/g },
             /* キャスト特化 */
@@ -10,6 +11,7 @@ $(document).ready(function () {
             { type: 'functionalName', regex: /(?<!\.)\b\w+(?=\()/g }
         ],
         c: [
+            { type: 'number', regex: /(?<=\s|[(),+\-*/])(\d+(\.\d+)?|\.\d+)(?=\s|[<(),+\-*/])/g },
             { type: 'string', regex: /(".*?")/g },
             { type: 'structure', regex: /\b(return|if|else|while|switch|case|default)\b/g },
             { type: 'type', regex: /\b(int|float|double|char|void)\b/g },
@@ -17,6 +19,7 @@ $(document).ready(function () {
             { type: 'functionalName', regex: /(?<!\.)\b\w+(?=\()/g }
         ],
         javascript: [
+            { type: 'number', regex: /(?<=\s|[(),+\-*/])(\d+(\.\d+)?|\.\d+)(?=\s|[<(),+\-*/])/g },
             { type: 'string', regex: /(["'`].*?["'`])/g },
             { type: 'structure', regex: /\b(return|if|else|for|while|import|export|class)\b/g },
             { type: 'type', regex: /\b(let|const|var)\b/g },

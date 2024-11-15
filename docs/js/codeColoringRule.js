9e3bf7c495bfc4ed9f75c8a83f22fb9f624b16f1
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2024-11-15 22:33:53 +0900

    website code highlight

--- docs/js/codeColoringRule.js
+++ docs/js/codeColoringRule.js
@@ -1,4 +1,15 @@
-Prism.plugins.autoloader.loadLanguages(['python']);
 document.addEventListener('DOMContentLoaded', function () {
-    Prism.highlightAll();
+    Prism.plugins.fileHighlight && Prism.plugins.fileHighlight.highlight();
+
+    Prism.hooks.add('after-highlight', function (env) {
+        if (env.element.matches('pre[data-src]')) {
+            Prism.highlightElement(env.element.querySelector('code'));
+        }
+    });
+
+    Prism.hooks.add('complete', function (env) {
+        if (env.element.parentNode.matches('pre[data-src][data-src-status="loaded"]')) {
+            Prism.plugins.matchBraces && Prism.plugins.matchBraces();
+        }
+    });
 });

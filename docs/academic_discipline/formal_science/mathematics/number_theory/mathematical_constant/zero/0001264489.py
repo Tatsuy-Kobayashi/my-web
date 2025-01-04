eb5b6eec2f7a20fa141587c68123f0d29fe99a78
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2025-01-05 05:01:19 +0900

    website higman_constant

--- docs/academic_discipline/formal_science/mathematics/number_theory/mathematical_constant/zero/0001264489.py
+++ docs/academic_discipline/formal_science/mathematics/number_theory/mathematical_constant/zero/0001264489.py
@@ -1,7 +1,24 @@
 import numpy as np
 import scipy
 
-# 円周率と近似値の誤差
-result, err = scipy.integrate.quad(lambda x: ((x ** 4) * (1 - x) ** 4) / (1 + x ** 2), 0, 1)
-print(f"積分 {result}")
-print(f"22 / 7 - π {22 / 7 - np.pi}")
+print(f"22/7 - π:  {22 / 7 - np.pi}")
+
+# scipy
+def scipy_int(n):
+    result, err = scipy.integrate.quad(lambda x: ((x ** 4) * (1 - x) ** 4) / (1 + x ** 2), 0, 1)
+    if n == 0:
+        return result
+    elif n == 1:
+        return err
+    else:
+        return None
+
+print(f"scipy積分:  {scipy_int(0)}")
+
+# mpmath
+import mpmath
+def mpmath_int(a):
+    result = mpmath.quad(lambda x: ((x ** 4) * (1 - x) ** 4) / (1 + x ** 2), [0, a])
+    return result
+
+print(f"mpmath積分: {mpmath_int(1)}")

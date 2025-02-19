cb1f9adafff48b9d09dfc8608531c09a7c777a81
Tatsuya KOBAYASHI <t4t5uy4k@gmail.com>
2025-02-20 01:59:26 +0900

    website set theory

---
+++ docs/academic_discipline/formal_science/mathematics/foundations_of_mathematics/set_theory/set_theory0.py
@@ -0,0 +1,13 @@
+print(f"集合型:   ", end='')
+print({1, 2, 3, 4, 5, 6, 7, 8, 9})
+print(f"内包表記: ", end='')
+print({i for i in range(1, 10)})
+print(f"関数:     {set(range(1, 10))}")
+
+my_set = set()  # 空集合
+for i in range(1, 10):
+    my_set.add(i)
+print(f"for文のブロックで定義: {my_set}")
+print({1, 1, 2})
+print(f"集合族: ", end='')
+print({1, frozenset({1, 2})})
